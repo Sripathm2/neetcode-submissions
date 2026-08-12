7ee8c79041eb8b2de523ca6dd92c6a1fc1290696
Sripath Mishra <22870429+Sripathm2@users.noreply.github.com>
2026-08-12 16:10:42 -0400

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(stones.length, Collections.reverseOrder());
+
+        for(int i = 0; i<stones.length; i++){
+            queue.add(stones[i]);
+        }
+
+        while(queue.size()>1){
+            int stone1 = queue.poll();
+            int stone2 = queue.poll();
+            if(stone1 == stone2){
+                continue;
+            }else{
+                queue.add(stone1-stone2);
+            }
+        }
+        if(queue.size() == 0){
+            return 0;
+        }else{
+            return queue.poll();
+        }
+    }
+}
