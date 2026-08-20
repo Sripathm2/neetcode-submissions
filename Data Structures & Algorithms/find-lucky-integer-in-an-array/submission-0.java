802e007451d0ab3ddab12d6f96c508fafcac0b86
Sripath Mishra <22870429+Sripathm2@users.noreply.github.com>
2026-08-20 11:05:08 -0400

    Add: find-lucky-integer-in-an-array - submission-0

---
+++ Data Structures & Algorithms/find-lucky-integer-in-an-array/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int count[]  = new int[501];
+        for(int num:arr){
+            count[num]++;
+        }
+        for(int i=count.length-1; i > 0; i--){
+            if(count[i] == i){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
