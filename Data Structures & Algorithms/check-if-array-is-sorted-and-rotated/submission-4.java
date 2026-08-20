2e9f564d08d0b8ca5eaa655c492f11909341a0a1
Sripath Mishra <22870429+Sripathm2@users.noreply.github.com>
2026-08-20 10:23:05 -0400

    Add: check-if-array-is-sorted-and-rotated - submission-4

---
+++ Data Structures & Algorithms/check-if-array-is-sorted-and-rotated/submission-4.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean check(int[] nums) {
+        boolean switched = false;
+        int pivot_point  = -1;
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] > nums[i+1]){
+                if(switched){
+                    return false;
+                }else{
+                    switched = true;
+                    pivot_point = i;
+                }
+            }
+        }
+
+        for(int i = 0; i <= pivot_point; i++){
+            for(int j = pivot_point+1; j < nums.length; j++){
+                if(nums[i]<nums[j]){
+                    // System.out.println(i + "  " + j);
+                    return false;
+                }
+            }
+
+        }
+
+
+
+        return true;
+    }
+}
